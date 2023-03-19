73097e1224b05f4148341bc1af2e1a00b68081a5
Alejandro Estrada <alejoestradam@yahoo.com>
2023-03-19 18:37:03 -0500

    useEffect to remove listeners

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    // const [coords, setCoords] = useState({x: 0, y: 0})
+
     useEffect(() => {
-      console.log("Message Mounted");
+      
+      const onMouseMove = ({x, y}) => {
+        const coords = {x ,y};
+        console.log({coords});
+      }
+
+      window.addEventListener('mousemove', onMouseMove );
     
       return () => {
-        console.log("Message unmounted");
+        window.removeEventListener('mousemove', onMouseMove);
       }
     }, [])
     
