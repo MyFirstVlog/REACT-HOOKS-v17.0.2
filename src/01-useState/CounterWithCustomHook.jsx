6a2353fc9c47203399e87fdcd4e151e0cd56cb00
Alejandro Estrada <alejoestradam@yahoo.com>
2023-03-19 17:42:26 -0500

    add custom counter hook

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,30 @@
+
+
+import React from 'react'
+import { useCounter } from '../hooks/useCounter'
+
+export const CounterWithCustomHook = () => {
+
+
+    const {counter, decrement, increment, reset} = useCounter();
+
+  return (
+    <>
+        <h1>Counter with Hook: {counter}</h1>
+        <hr/>
+
+        <button 
+            className='btn btn-primary'
+            onClick={() => increment()}
+        > +1 </button>
+        <button 
+            className='btn btn-primary'
+            onClick={reset}
+        > reset </button>
+        <button 
+            className='btn btn-primary'
+            onClick={() => decrement()}
+        > -1 </button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './01-useState/CounterApp';
+// import { CounterApp } from './01-useState/CounterApp';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterApp/>
+    <CounterWithCustomHook/>
   </React.StrictMode>,
   document.getElementById('root')
 );
