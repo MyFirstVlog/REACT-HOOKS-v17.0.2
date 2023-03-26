c99556b76aee6ab1934857ec27a09b248eda7f60
Alejandro Estrada <alejoestradam@yahoo.com>
2023-03-25 20:20:12 -0500

    using useRef to handle focus on inputs

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleCustomHooks } from './examples/MultipleCustomHook';
+// import { MultipleCustomHooks } from './examples/MultipleCustomHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HooksApp } from './HooksApp';
 
 import './index.css';
+import { FocusScreen } from './useRef/FocusScreen';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MultipleCustomHooks/>
+    <FocusScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
