58a8141fbad7d804cce51f71e65a5fcef168e35c
Alejandro Estrada <alejoestradam@yahoo.com>
2023-03-25 21:49:27 -0500

    solving last case scenario usign useCallback with args

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
   
     const [counter, setCounter] = useState(20);
 
-    const increment = () => {
-        setCounter(counter + 1);
-    }
+    const increment = useCallback(
+      (number) => {
+        setCounter((value) => value + number);
+      },
+      [],
+    );
+    
 
     return (
-    <>
+    <>  
         <h1>useCallback Hook: {counter}</h1>
         <hr />
 
